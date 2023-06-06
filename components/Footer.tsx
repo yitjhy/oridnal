559fb6eac2357e8b978a1bb1412d81f5b0e8a7da
yitjhy@126.com <yitjhy@126.com>
2023-06-06 20:07:47 +0800

    wip: header

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,38 +1,15 @@
-import { GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon, TwitterLogoIcon } from '@radix-ui/react-icons'
 
 const Footer = () => {
   return (
     <footer className="p-3 sm:p-5 md:p-10 w-full max-w-[88rem] mx-auto">
       <div className="w-full bg-black rounded-md">
         <div className="p-4 sm:p-6 md:p-12 pb-8 flex flex-col">
-          <a href="https://hiro.so" aria-label="Hiro Systems">
-            <div className="hiro w-[54px] h-[54px]"></div>
+          <a href="/">
+            <img src="/logo2.svg" alt="Hiro Ordinals Beta" />
           </a>
           {/* todo: link chainhooks */}
-          <p className="my-4 md:my-6 text-sm text-neutral-0">
-            This Ordinals Explorer is using the{" "}
-            <a
-              className="underline"
-              href="https://docs.hiro.so/ordinals"
-            >
-              Hiro Ordinals API
-            </a>{" "}
-              powered by{" "}
-            <a
-              className="underline"
-              href="https://github.com/hirosystems/chainhook"
-            >
-              Chainhooks
-            </a>
-            .
-          </p>
           <div className="mt-10 flex flex-col md:flex-row justify-between text-center text-xs text-neutral-300 space-y-4 md:space-y-0">
-            <div>
-              {/* todo: links */}
-              <a href="https://www.hiro.so/patent-pledge">Patent Pledge</a>{" "}
-              &mdash;{" "}
-              <a href="https://www.hiro.so/terms-privacy">Terms & Privacy</a>
-            </div>
             <div className="space-x-3">
               <a href="https://twitter.com/hirosystems">
                 {/* todo: find filled twitter icon */}
@@ -49,7 +26,7 @@ const Footer = () => {
         </div>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,63 +1,81 @@
-"use client";
+'use client'
 
-import { ArrowTopRightIcon } from "@radix-ui/react-icons";
-import { ArrowUpRight } from "lucide-react";
-import Link from "next/link";
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "./HoverCard";
+import { ArrowTopRightIcon } from '@radix-ui/react-icons'
+import { ArrowUpRight } from 'lucide-react'
+import Link from 'next/link'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './HoverCard'
+import { usePathname } from 'next/navigation'
 
 const Header = ({ children }: { children?: React.ReactNode }) => {
+  const pathname = usePathname()
   return (
     <header className="flex justify-between px-6 md:px-10 py-8 w-full max-w-[88rem] mx-auto">
       <a href="/">
-        <img src="/logo.svg" alt="Hiro Ordinals Beta" />
+        <img src="/logo2.svg" alt="Hiro Ordinals Beta" />
       </a>
       {children}
       {/* todo: explore button, stats, hiro.so */}
       <div className="hidden sm:block">
-        <HoverCard openDelay={0}>
-          <HoverCardTrigger className="px-3.5 py-2.5 rounded-md cursor-default hover:bg-neutral-0 select-none">
-            Explore
-          </HoverCardTrigger>
-          <HoverCardContent className="flex flex-col p-1">
-            <Link
-              href="/explore"
-              className="px-3.5 py-2.5 rounded-md hover:bg-neutral-0"
-            >
-              All inscriptions
-            </Link>
-            <Link
-              href="/period"
-              className="px-3.5 py-2.5 rounded-md hover:bg-neutral-0"
-            >
-              By halving period
-            </Link>
-          </HoverCardContent>
-        </HoverCard>
-        <HoverCard openDelay={0}>
-          <HoverCardTrigger className="opacity-50 cursor-not-allowed">
-            <span
-              // todo: change back to Link
-              // href="/stats"
-              className="px-3.5 py-2.5 rounded-md hover:bg-neutral-0"
-            >
-              Stats
-            </span>
-          </HoverCardTrigger>
-          <HoverCardContent className="px-2">
-            Under Construction 🚧
-          </HoverCardContent>
-        </HoverCard>
+        {/*<HoverCard openDelay={0}>*/}
+        {/*  <HoverCardTrigger className="px-3.5 py-2.5 rounded-md cursor-default hover:bg-neutral-0 select-none">*/}
+        {/*    Explore*/}
+        {/*  </HoverCardTrigger>*/}
+        {/*  <HoverCardContent className="flex flex-col p-1">*/}
+        {/*    <Link*/}
+        {/*      href="/explore"*/}
+        {/*      className="px-3.5 py-2.5 rounded-md hover:bg-neutral-0"*/}
+        {/*    >*/}
+        {/*      All inscriptions*/}
+        {/*    </Link>*/}
+        {/*    <Link*/}
+        {/*      href="/period"*/}
+        {/*      className="px-3.5 py-2.5 rounded-md hover:bg-neutral-0"*/}
+        {/*    >*/}
+        {/*      By halving period*/}
+        {/*    </Link>*/}
+        {/*  </HoverCardContent>*/}
+        {/*</HoverCard>*/}
+        {/*<HoverCard openDelay={0}>*/}
+        {/*  <HoverCardTrigger className="opacity-50 cursor-not-allowed">*/}
+        {/*    <span*/}
+        {/*      // todo: change back to Link*/}
+        {/*      // href="/stats"*/}
+        {/*      className="px-3.5 py-2.5 rounded-md hover:bg-neutral-0"*/}
+        {/*    >*/}
+        {/*      Stats*/}
+        {/*    </span>*/}
+        {/*  </HoverCardTrigger>*/}
+        {/*  <HoverCardContent className="px-2">*/}
+        {/*    Under Construction 🚧*/}
+        {/*  </HoverCardContent>*/}
+        {/*</HoverCard>*/}
         <Link
-          target="_blank"
-          href="https://hiro.so"
-          className="px-3.5 py-2.5 rounded-md text-neutral-300 hover:text-neutral-500"
+          href="/"
+          className={`px-[1.5rem] py-2.5 rounded-md ${
+            pathname === '/' ? 'text-[#F5BD07]' : 'text-[#656565]'
+          } hover:text-[#F5BD07]`}
         >
-          hiro.so
-          <ArrowUpRight className="inline" size={16} />
+          Home
+        </Link>
+        <Link
+          href="/explore"
+          className={`px-[1.5rem] py-2.5 rounded-md ${
+            pathname === '/explore' ? 'text-[#F5BD07]' : 'text-[#656565]'
+          } hover:text-[#F5BD07]`}
+        >
+          Explore
+        </Link>
+        <Link
+          href="/block-chain"
+          className={`px-[1.5rem] py-2.5 rounded-md ${
+            pathname === '/block-chain' ? 'text-[#F5BD07]' : 'text-[#656565]'
+          } hover:text-[#F5BD07]`}
+        >
+          Blockchain
         </Link>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
