e28886ca9abcc660d5537bd0af117b15d6b9fbc1
yitjhy@126.com <yitjhy@126.com>
2023-06-06 20:18:09 +0800

    wip: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { GitHubLogoIcon, TwitterLogoIcon } from '@radix-ui/react-icons'
 const Footer = () => {
   return (
     <footer className="p-3 sm:p-5 md:p-10 w-full max-w-[88rem] mx-auto">
-      <div className="w-full bg-black rounded-md">
+      <div className="w-full bg-black rounded-md flex justify-between">
         <div className="p-4 sm:p-6 md:p-12 pb-8 flex flex-col">
           <a href="/">
             <img src="/logo2.svg" alt="Hiro Ordinals Beta" />
@@ -18,10 +18,30 @@ const Footer = () => {
               <a href="https://github.com/hirosystems">
                 <GitHubLogoIcon className="inline-block w-[18px] h-[18px] text-neutral-0" />
               </a>
-              <a href="https://hiro.so" className="inline-block">
-                &copy; {new Date().getFullYear()} Hiro Systems PBC
-              </a>
+              {/*<a href="https://hiro.so" className="inline-block">*/}
+              {/*  &copy; {new Date().getFullYear()} Hiro Systems PBC*/}
+              {/*</a>*/}
             </div>
+          </div>
+        </div>
+        <div className="p-4 sm:p-6 md:p-12 pb-8 flex  text-[#929292] gap-x-[3rem] text-[14px]">
+          <div className="grid gap-y-[0.5rem]">
+            <div>Product</div>
+            <div>Explorer</div>
+            <div>Mining pool</div>
+          </div>
+          <div className="grid gap-y-[0.5rem]">
+            <div>Product</div>
+            <div>Explorer</div>
+            <div>Mining pool</div>
+            <div>App</div>
+          </div>
+          <div className="grid gap-y-[0.5rem]">
+            <div>Product</div>
+            <div>Explorer</div>
+            <div>Mining pool</div>
+            <div>App</div>
+            <div>Mining pool</div>
           </div>
         </div>
       </div>
