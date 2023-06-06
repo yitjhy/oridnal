6a90b69d83b915500e5418597d7a5c3635de4857
yitjhy@126.com <yitjhy@126.com>
2023-06-06 20:23:45 +0800

    wip: footer

--- components/GalleryPreview.tsx
+++ components/GalleryPreview.tsx
@@ -1,56 +1,53 @@
-import { useSetAtom } from "jotai";
-import { useEffect } from "react";
-import useSWR from "swr";
+import { useSetAtom } from 'jotai'
+import { useEffect } from 'react'
+import useSWR from 'swr'
 
-import { API_URL } from "../lib/constants";
-import { fetcher } from "../lib/helpers";
-import { lastInscriptionDataAtom } from "../lib/store";
+import { API_URL } from '../lib/constants'
+import { fetcher } from '../lib/helpers'
+import { lastInscriptionDataAtom } from '../lib/store'
 
-import { InscriptionResponse, ListResponse } from "../lib/types";
-import InscriptionCard from "./InscriptionCard";
-import Table, {TableColumn} from "@/components/table/Table";
-import moment from "moment";
+import { InscriptionResponse, ListResponse } from '../lib/types'
+import InscriptionCard from './InscriptionCard'
+import Table, { TableColumn } from '@/components/table/Table'
+import moment from 'moment'
 
 const columns: TableColumn<InscriptionResponse>[] = [
-    {
-        name: 'Inscritions',
-        sortable: false,
-        key: 'address',
-        render: (data) => <>Inscritions#{data?.number}</>,
-    },
-    {
-        name: 'Transfer',
-        sortable: false,
-        key: 'timestamp',
-        render: (data) => <>{data?.timestamp}</>,
-    },
-    {
-        name: 'Time',
-        sortable: false,
-        key: 'timestamp',
-        render: (data) => <>{moment(data?.timestamp).format('YYYY.MM.DD HH:mm:ss')}</>,
-    },
+  {
+    name: 'Inscritions',
+    sortable: false,
+    key: 'address',
+    render: (data) => <span>Inscritions#{data?.number}</span>,
+  },
+  {
+    name: 'Transfer',
+    sortable: false,
+    key: 'timestamp',
+    render: (data) => <span>{data?.timestamp}</span>,
+  },
+  {
+    name: 'Time',
+    sortable: false,
+    key: 'timestamp',
+    render: (data) => <span>{moment(data?.timestamp).format('YYYY.MM.DD HH:mm:ss')}</span>,
+  },
 ]
 const GalleryPreview = () => {
-  const setLastInscriptionData = useSetAtom(lastInscriptionDataAtom);
+  const setLastInscriptionData = useSetAtom(lastInscriptionDataAtom)
 
-  const { data, error, isLoading } = useSWR<ListResponse<InscriptionResponse>>(
-    `${API_URL}/inscriptions`,
-    fetcher
-  );
+  const { data, error, isLoading } = useSWR<ListResponse<InscriptionResponse>>(`${API_URL}/inscriptions`, fetcher)
 
   useEffect(() => {
     if (data?.results?.length) {
       setLastInscriptionData({
         number: data.results[0].number.toString(),
         date: new Date(data.results[0].timestamp),
-      });
+      })
     }
-  }, [data, setLastInscriptionData]);
+  }, [data, setLastInscriptionData])
 
-  if (error) return <span>Something went wrong ʕ•̠͡•ʔ</span>;
+  if (error) return <span>Something went wrong ʕ•̠͡•ʔ</span>
 
-  const previews = data ? data.results : Array(12).fill(null); // skeleton values
+  const previews = data ? data.results : Array(12).fill(null) // skeleton values
 
   return (
     <>
@@ -58,20 +55,14 @@ const GalleryPreview = () => {
         {/*{previews.slice(0, 12).map((i, index) => (*/}
         {/*  <InscriptionCard key={index} inscription={i} />*/}
         {/*))}*/}
-          <div>
-              {
-                  previews && <Table data={previews} columns={columns} />
-              }
-          </div>
-
-
+        <div>{previews && <Table data={previews} columns={columns} />}</div>
       </div>
       {/* todo: re-add figma link to full feed */}
       {/* <div className="my-4 text-center underline">
         <a href="/inscriptions">More</a>
       </div> */}
     </>
-  );
-};
+  )
+}
 
-export default GalleryPreview;
+export default GalleryPreview
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,5 @@
-export const TILE_SIZE = 16;
+export const TILE_SIZE = 16
 
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ?? "https://api.hiro.so/ordinals/v1";
+  // process.env.NEXT_PUBLIC_API_URL ?? "https://api.hiro.so/ordinals/v1";
+  process.env.NEXT_PUBLIC_API_URL ?? 'https://api.ordinalscan.net/ordinals/v1'
